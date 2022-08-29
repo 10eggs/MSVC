a6e2f39aa4429aad0d5ca743a5ae483cadf59d5e
10eggs <pawlak325@gmail.com>
2022-08-29 11:31:17 +0200

    URLs updated for all services, docker files rebuilded

--- moderation/index.js
+++ moderation/index.js
@@ -10,7 +10,7 @@ app.post('/events',async (req,res)=>{
     if(type==='CommentCreated'){
         const commentStatus = data.content.includes('oranges')?'rejected':'approved';
 
-        await axios.post('http://localhost:4005/events',{
+        await axios.post('http://event-bus-srv:4005/events',{
             type: 'CommentModerated',
             data: {
                 id: data.id,
